5fee912f6ddd3ab62f4494e9c2d6094b6c394c72
donnelly-dII <donnellyd215@gmail.com>
2020-12-18 09:52:20 -0500

    Completed NotificationService create_new_notification

--- sql_learning_app/api_vi/entity/notification/notification_exceptions.py
+++ sql_learning_app/api_vi/entity/notification/notification_exceptions.py
@@ -10,3 +10,10 @@ class InvalidSourceId(BaseApiException):
         super().__init__(f'No Source entiity with id {source_id} could be found. '
                          f'Ensure that this source is an object of type "Entity"')
         self.status_code = 404
+
+
+class InvalidNotificationType(BaseApiException):
+    def __init__(self, type_id: int):
+        super().__init__(f'No Notification Type  with id {type_id} could be found. Notifications can only be '
+                         f'created from active and valid NtificationTypes')
+        self.status_code = 404
